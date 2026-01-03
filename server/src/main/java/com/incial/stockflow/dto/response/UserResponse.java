d06e5c02f7e89d39c0296e71fd9bbb8794a8fcad
Skywalker690 <sanjoksiby0@gmail.com>
2026-01-03 21:10:53 +0530

    feat(dto): implement request and response DTOs across APIs

---
+++ server/src/main/java/com/incial/stockflow/dto/response/UserResponse.java
@@ -0,0 +1,21 @@
+package com.incial.stockflow.dto.response;
+
+import com.incial.stockflow.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private UUID id;
+    private String name;
+    private String email;
+    private UserRole role;
+    private UUID outletId;
+}
